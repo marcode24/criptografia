bd49b25d87e8ac44b8eb1fe5606672d768dd0edb
marco <marco24cruz08@gmail.com>
2023-10-22 22:15:22 -0600

    :lipstick: update UI styles files

--- src/public/src/js/base64.js
+++ src/public/src/js/base64.js
@@ -3,8 +3,7 @@ const d = document;
 const $text = d.getElementById('text');
 const $btnAccept = d.getElementById('accept');
 const $chkType = d.getElementById('type-encrypt');
-
-const $resultsContainer = d.querySelector('.results');
+const $form = d.getElementById('form');
 
 let valuechkType = $chkType.checked;
 
@@ -28,6 +27,11 @@ const fetchData = async (url, data) => {
   return result;
 };
 
+const clearResults = () => {
+  const results = d.querySelectorAll('.results');
+  if (results) results.forEach((result) => result.remove());
+};
+
 const encode = async (data) => {
   const words = [
     'Hola',
@@ -38,13 +42,26 @@ const encode = async (data) => {
   ];
 
   const buildResults = (original, result) => `
-    <div class="result-wrapper">
-      <span class="subtitle">Texto original</span>
-      <span class="result">${original}</span>
-      <span class="subtitle">Cifrado en base 64</span>
-      <span class="result">${result}</span>
+    <div class="results">
+      <div class="result-wrapper">
+        <span class="subtitle">Texto original</span>
+        <span class="result">${original}</span>
+        <span class="subtitle">Cifrado en base 64</span>
+        <span class="result">${result}</span>
+      </div>
     </div>
     `;
+
+  const separatorElement = () => {
+    const separator = `
+      <div class="results">
+        <div class="result-wrapper">
+          <span class="subtitle separator">MÁS EJEMPLOS</span>
+        </div>
+      </div>
+    `;
+    return separator;
+  };
 
   const promises = words.map(async (word) => {
     const result = await fetchData('/encode', word);
@@ -60,30 +77,54 @@ const encode = async (data) => {
 
   const fetchUser = await fetchData('/encode', data);
   const resultsUser = buildResults(data, fetchUser.result);
-  $resultsContainer.innerHTML = resultsUser + resultsMapped;
+  clearResults();
+  $form.insertAdjacentHTML('afterend', resultsUser + separatorElement() + resultsMapped);
 };
 
 const decode = async (data) => {
   const { result, original } = await fetchData('/decode', data);
+  clearResults();
   const results = `
-    <div class="result-wrapper">
-      <span class="subtitle">Cadena original</span>
-      <span class="result">${original}</span>
-      <span class="subtitle">Descifrado en base 64</span>
-      <span class="result">${result}</span>
+    <div class="results">
+      <div class="result-wrapper">
+        <span class="subtitle">Cadena original</span>
+        <span class="result">${original}</span>
+        <span class="subtitle">Descifrado en base 64</span>
+        <span class="result">${result}</span>
+      </div>
     </div>
   `;
-  $resultsContainer.innerHTML = results;
+  $form.insertAdjacentHTML('afterend', results);
 };
 
-$btnAccept.addEventListener('click', () => {
+const showErrorMeessage = (message) => {
+  const errors = `
+    <div class="errors">
+      <span class="error">${message}</span>
+    </div>
+  `;
+  $text.insertAdjacentHTML('afterend', errors);
+};
+
+const clearErrors = () => {
+  const errors = d.querySelector('.errors');
+  if (errors) errors.remove();
+};
+
+$form.addEventListener('submit', (e) => {
+  e.preventDefault();
   const text = $text.value.trim().replace(/\s+/g, ' ');
 
   if (!text || text.length === 0) {
-    // eslint-disable-next-line no-alert
-    alert('Debes ingresar un texto');
+    clearErrors();
+    showErrorMeessage('* Debes ingresar un texto');
     return;
   }
 
   valuechkType ? decode(text) : encode(text);
+});
+
+$form.addEventListener('input', () => {
+  const errors = d.querySelector('.errors');
+  if (errors) errors.remove();
 });
--- src/public/src/js/main.js
+++ src/public/src/js/main.js
@@ -3,8 +3,7 @@ const d = document;
 const $text = d.getElementById('text');
 const $btnAccept = d.getElementById('accept');
 const $chkType = d.getElementById('type-encrypt');
-
-const $resultsContainer = d.querySelector('.results');
+const $form = d.getElementById('form');
 
 let valuechkType = $chkType.checked;
 
@@ -28,46 +27,77 @@ const fetchData = async (url, data) => {
   return result;
 };
 
+const clearResults = () => {
+  const results = d.querySelector('.results');
+  if (results) results.remove();
+};
+
 const encrypt = async (data) => {
   const result = await fetchData('/encrypt', data);
+  clearResults();
   const { encoded, encrypted } = result;
   const results = `
-    <div class="result-wrapper">
-      <span class="subtitle">Texto original</span>
-      <span class="result">${data}</span>
-      <span class="subtitle">Cifrado en base 64</span>
-      <span class="result">${encoded}</span>
-      <span class="subtitle">Cifrado en AES</span>
-      <span class="result">${encrypted}</span>
+    <div class="results">
+      <div class="result-wrapper">
+        <span class="subtitle">Texto original</span>
+        <span class="result">${data}</span>
+        <span class="subtitle">Cifrado en base 64</span>
+        <span class="result">${encoded}</span>
+        <span class="subtitle">Cifrado en AES</span>
+        <span class="result">${encrypted}</span>
+      </div>
     </div>
   `;
-  $resultsContainer.innerHTML = results;
+  $form.insertAdjacentHTML('afterend', results);
 };
 
 const decrypt = async (data) => {
   const result = await fetchData('/decrypt', data);
+  clearResults();
   const { decrypted, decoded } = result;
   const results = `
-    <div class="result-wrapper">
-      <span class="subtitle">Cadena original</span>
-      <span class="result">${data}</span>
-      <span class="subtitle">Descifrado en AES</span>
-      <span class="result">${decrypted}</span>
-      <span class="subtitle">Descifrado en base 64</span>
-      <span class="result">${decoded}</span>
+    <div class="results">
+      <div class="result-wrapper">
+        <span class="subtitle">Cadena original</span>
+        <span class="result">${data}</span>
+        <span class="subtitle">Descifrado en AES</span>
+        <span class="result">${decrypted}</span>
+        <span class="subtitle">Descifrado en base 64</span>
+        <span class="result">${decoded}</span>
+      </div>
     </div>
   `;
-  $resultsContainer.innerHTML = results;
+  $form.insertAdjacentHTML('afterend', results);
 };
 
-$btnAccept.addEventListener('click', () => {
+const showErrorMeessage = (message) => {
+  const errors = `
+    <div class="errors">
+      <span class="error">${message}</span>
+    </div>
+  `;
+  $text.insertAdjacentHTML('afterend', errors);
+};
+
+const clearErrors = () => {
+  const errors = d.querySelector('.errors');
+  if (errors) errors.remove();
+};
+
+$form.addEventListener('submit', (e) => {
+  e.preventDefault();
   const text = $text.value.trim().replace(/\s+/g, ' ');
 
   if (!text || text.length === 0) {
-    // eslint-disable-next-line no-alert
-    alert('Debes ingresar un texto');
+    clearErrors();
+    showErrorMeessage('* Debes ingresar un texto');
     return;
   }
 
   valuechkType ? decrypt(text) : encrypt(text);
+});
+
+$form.addEventListener('input', () => {
+  const errors = d.querySelector('.errors');
+  if (errors) errors.remove();
 });
